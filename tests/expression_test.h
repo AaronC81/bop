2a3d4a683cb4de780a25570db7f06b239696cc2c
Aaron Christiansen <aaronc20000@gmail.com>
2021-02-15 20:25:10 +0000

    Add operator_add test

--- tests/expression_test.h
+++ tests/expression_test.h
@@ -10,10 +10,27 @@ START_TEST(expression_evaluate_number) {
 }
 END_TEST
 
+START_TEST(expression_evaluate_operator_add) {
+    struct expression l = expression_new_number((struct expression_number){ .n = 1.5 });
+    struct expression r1 = expression_new_number((struct expression_number){ .n = 2.1 });
+    struct expression r2 = expression_new_number((struct expression_number){ .n = 4.9 });
+    struct expression r = expression_new_operator_add((struct expression_operator_add){ .left = &r1, .right = &r2 });
+    struct expression add = expression_new_operator_add((struct expression_operator_add){ .left = &l, .right = &r });
+    
+    bop_number n;
+    fail_unless(expression_evaluate(&r, &n) == RESULT_OK);
+    fail_unless(n == 7.0);
+    fail_unless(expression_evaluate(&add, &n) == RESULT_OK);
+    fail_unless(n == 8.5);
+}
+END_TEST
+
+
 TCase* expression_tcase() {
     TCase* tcase = tcase_create("expression");
 
     tcase_add_test(tcase, expression_evaluate_number);
+    tcase_add_test(tcase, expression_evaluate_operator_add);
 
     return tcase;
 }
